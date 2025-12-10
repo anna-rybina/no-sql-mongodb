56b5832df0f4da41b38e8a74a79515217219f9b1
Neto test <annav.rybina@yandex.ru>
2025-12-10 03:20:02 +0300

    Initial commit

---
+++ src/test/java/com/github/annarybina/nosqlmongo/NosqlMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.annarybina.nosqlmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
